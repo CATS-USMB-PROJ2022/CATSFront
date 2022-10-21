a363bbb565aa4c02a06f6962dd3280bd6857faf7
rakotoew <55093676+rakotoew@users.noreply.github.com>
2022-10-21 15:34:16 +0200

    Add example for bar chart

--- cats-front/src/app/pages/call-time-diagram/call-time-diagram.component.ts
+++ cats-front/src/app/pages/call-time-diagram/call-time-diagram.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ChartConfiguration, ChartData, ChartEvent, ChartType} from "chart.js";
+import {BaseChartDirective} from "ng2-charts";
+import data from './../../../assets/json/data.json';
 
 @Component({
   selector: 'app-call-time-diagram',
@@ -6,6 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./call-time-diagram.component.css']
 })
 export class CallTimeDiagramComponent implements OnInit {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10
+      }
+    },
+    plugins: {
+      legend: {
+        display: true,
+      }
+    }
+  };
+
+  public barChartType: ChartType = 'bar';
+
+  public barChartData: ChartData<'bar'> = {
+    labels: data.Time_data.label,
+    datasets: data.Time_data.nb
+  };
+
+  // events
+  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
 
   constructor() { }
 
