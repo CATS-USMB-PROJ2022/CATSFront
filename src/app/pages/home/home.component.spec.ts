bc34e8fd1d0e2a0f99e092dee3116fdeec7185d9
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2022-12-14 14:57:51 +0100

    test:ajout test HomeComponent

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import {CallService} from "../../service/call.service";
+import {of} from "rxjs";
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  let callService: CallService;
+
   beforeEach(async () => {
+    callService = jasmine.createSpyObj(CallService, {
+      postNumberCall: of('http://localhost:8080/Home'),
+    });
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      providers: [{ provide: CallService, useValue: callService }],
     })
     .compileComponents();
 
@@ -19,5 +27,6 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(callService.postNumberCall).toHaveBeenCalled();
   });
 });
--- src/app/service/call.service.spec.ts
+++ src/app/service/call.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CallService } from './call.service';
+import {HttpClient} from "@angular/common/http";
 
 describe('CallService', () => {
   let service: CallService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [],
+      providers: [{ provide: HttpClient, useValue: {} }],
+    });
     service = TestBed.inject(CallService);
   });
 
