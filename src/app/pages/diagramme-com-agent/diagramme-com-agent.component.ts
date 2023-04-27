f9a026bdbf0ab339dba7533752201beb3f478408
Coco1900 <46676720+Coco1900@users.noreply.github.com>
2023-04-27 13:22:36 +0200

    fix: label print agent

--- src/app/pages/diagramme-com-agent/diagramme-com-agent.component.ts
+++ src/app/pages/diagramme-com-agent/diagramme-com-agent.component.ts
@@ -104,6 +104,9 @@ export class DiagrammeComAgentComponent implements OnInit, OnChanges, OnDestroy 
       plugins: {
         legend: {
           display: false
+        },
+        datalabels:{
+          display:false
         }
       }
     }
