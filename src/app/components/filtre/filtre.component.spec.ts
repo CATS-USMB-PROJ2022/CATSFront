3116314a283838553bbd3aa137a90ee485400251
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2023-04-27 09:14:54 +0200

    test(filtre): add test on filtre component

--- src/app/components/filtre/filtre.component.spec.ts
+++ src/app/components/filtre/filtre.component.spec.ts
@@ -1,22 +1,69 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {FiltreComponent} from "./filtre.component";
+import {Component, DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 describe('FiltreComponent', () => {
   let component: FiltreComponent;
   let fixture: ComponentFixture<FiltreComponent>;
+  let hostComponent: TestHostComponent;
+  let hostFixture: ComponentFixture<TestHostComponent>;
+  let resetBtn: DebugElement;
+  let confirmBtn: DebugElement;
+
+  @Component({
+    selector: 'app-test-host',
+    template: `
+      <filtre [reinitialisation]="reinit" [confirmation]="confirm">
+        <p>Component</p>
+      </filtre>
+    `,
+  })
+  class TestHostComponent {
+    public reinit = jest.fn();
+    public confirm = jest.fn();
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        FiltreComponent
+        FiltreComponent, TestHostComponent
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FiltreComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    hostFixture = TestBed.createComponent(TestHostComponent);
+    hostComponent = hostFixture.componentInstance;
+    hostFixture.detectChanges();
+
+    resetBtn = hostFixture.debugElement.query(By.css('#button-reinit'));
+    confirmBtn = hostFixture.debugElement.query(By.css('#button-conf'));
   });
 
   it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have inputs', () => {
+    expect(component.reinitialisation).toBeDefined();
+    expect(component.confirmation).toBeDefined();
+  });
+
+  it('should call the reinitialisation function on reset button click', () => {
+    resetBtn.triggerEventHandler('click', null);
+    expect(hostComponent.reinit).toHaveBeenCalled();
+  });
+
+  it('should call the confirmation function on confirm button click', () => {
+    confirmBtn.triggerEventHandler('click', null);
+    expect(hostComponent.confirm).toHaveBeenCalled();
+  });
+
+  it('should display the transcluded content', () => {
+    const transcludedContent = hostFixture.debugElement.query(By.css('p'));
+    expect(transcludedContent.nativeElement.textContent.trim()).toBe('Component');
+  });
+
 });
--- src/app/pages/diagramme-repartition-appel/diagramme-repartition-appel.component.spec.ts
+++ src/app/pages/diagramme-repartition-appel/diagramme-repartition-appel.component.spec.ts
@@ -1,11 +1,20 @@
 import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {DiagrammeRepartitionAppelComponent} from "./diagramme-repartition-appel.component";
 import {HttpClient, HttpHandler} from "@angular/common/http";
+import {of} from "rxjs";
+import {ValeursService} from "../../service/valeurs.service";
+import {PostService} from "../../service/post.service";
+import {CaisseRegionaleService} from "../../service/caisse-regionale.service";
 
 describe('DiagrammeRepartitionAppelComponent', () => {
   let component: DiagrammeRepartitionAppelComponent;
   let fixture: ComponentFixture<DiagrammeRepartitionAppelComponent>;
+
+  let mockCaisseRegionaleService: any;
+  let mockPostService: any;
+  let mockValeursService: any;
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
       providers: [
         HttpClient,
@@ -26,4 +35,5 @@ describe('DiagrammeRepartitionAppelComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
