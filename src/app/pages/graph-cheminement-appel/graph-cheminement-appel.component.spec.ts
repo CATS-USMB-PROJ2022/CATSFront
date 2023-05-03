8da8d35c40dd5392b3495f66246d0f9fd224c38d
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2023-05-03 17:19:09 +0200

    test(): correction on test files

--- src/app/pages/graph-cheminement-appel/graph-cheminement-appel.component.spec.ts
+++ src/app/pages/graph-cheminement-appel/graph-cheminement-appel.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GraphCheminementAppelComponent } from './graph-cheminement-appel.component';
+import {HttpClient, HttpHandler} from "@angular/common/http";
 
 describe('GraphCheminementAppelComponent', () => {
   let component: GraphCheminementAppelComponent;
@@ -8,7 +9,13 @@ describe('GraphCheminementAppelComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GraphCheminementAppelComponent ]
+      declarations: [ GraphCheminementAppelComponent ],
+      providers: [
+        HttpClient,
+        {provide: 'globalUrl', useValue: 'http://localhost:8080/'},
+        HttpHandler,
+        {provide: 'globalUrl', useValue: 'http://localhost:8080/'},
+      ],
     })
     .compileComponents();
 
