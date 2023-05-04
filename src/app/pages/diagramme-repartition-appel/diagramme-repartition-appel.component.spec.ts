4f3c3955c331f5c9196938dfdd31dee07959b55f
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2023-05-04 11:31:54 +0200

    feat(diag repartition): add test

--- src/app/pages/diagramme-repartition-appel/diagramme-repartition-appel.component.spec.ts
+++ src/app/pages/diagramme-repartition-appel/diagramme-repartition-appel.component.spec.ts
@@ -1,10 +1,16 @@
 import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {DiagrammeRepartitionAppelComponent} from "./diagramme-repartition-appel.component";
 import {HttpClient, HttpHandler} from "@angular/common/http";
+import {CaisseRegionaleService} from "../../service/caisse-regionale.service";
+import {PostService} from "../../service/post.service";
+import {ValeursService} from "../../service/valeurs.service";
 
 describe('DiagrammeRepartitionAppelComponent', () => {
   let component: DiagrammeRepartitionAppelComponent;
   let fixture: ComponentFixture<DiagrammeRepartitionAppelComponent>;
+  let caisseRegionaleService: CaisseRegionaleService;
+  let postService: PostService;
+  let valeursService: ValeursService;
 
   beforeEach(async () => {
 
@@ -14,6 +20,9 @@ describe('DiagrammeRepartitionAppelComponent', () => {
         {provide: 'globalUrl', useValue: 'http://localhost:8080/'},
         HttpHandler,
         {provide: 'globalUrl', useValue: 'http://localhost:8080/'},
+        CaisseRegionaleService,
+        PostService,
+        valeursService
       ],
       declarations: [
         DiagrammeRepartitionAppelComponent
@@ -23,10 +32,17 @@ describe('DiagrammeRepartitionAppelComponent', () => {
     fixture = TestBed.createComponent(DiagrammeRepartitionAppelComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    caisseRegionaleService = TestBed.inject(CaisseRegionaleService);
+    postService = TestBed.inject(PostService);
+    valeursService = TestBed.inject(ValeursService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should ', () => {
+
+  });
 });
